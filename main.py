5d42a41707b13a604aad192e6b1b22e515799520
drlbminh <38910315+drlbminh@users.noreply.github.com>
2019-03-18 19:02:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# import pdb
-# pdb.set_trace()
 import os
 import time
 time.sleep(1)
-#os.system('pulseaudio -D')
-#time.sleep(1)
-#os.system('sudo pacmd set-source-volume 1 200000')
-#time.sleep(1)
-#os.system('sudo pulseaudio --start')
-#time.sleep(1)
 
 
 import dem
